57fbadbe0e84bad1d778a4f1a4d422208b339537
Alina Vlasova <alina_vlasova_01@bk.ru>
2024-10-16 21:04:40 +0300

    create App and MainPage component, add Logo.svg

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/Logo.svg">
+    <MainPage title="Экскурсии по всему миру"/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script type="module">
+import MainPage from "./components/MainPage.vue";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MainPage,
   }
 }
 </script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'MainPage',
+  data() {
+    return {
+      title: 'Экскурсии по всему миру',
+      nameExcursion: 'Введите название экскурсии',
+      nameTown: 'Выбрать город'
+    }
+  },
+  methods: {
+    getExcursion(str) {
+      this.nameExcursion = str
+    },
+    getExcursionByTown(str) {
+      this.nameTown = str
+    }
+  }
+}
+
+</script>
+
+<template>
+<div>
+  <h1>{{ title }}</h1>
+  <input  type="text" v-on:keyup="getExcursion()" value="this.nameExcursion">
+  <input type="text" v-on:keyup="getExcursionByTown()" value="this.nameTown">
+</div>
+</template>
+
+<style scoped>
+
+</style>
